5789ea45726c64a27b63046bcaefdd6dfd2c59b9
jhemedin <jason.hemedinger@gmail.com>
2019-08-13 14:47:08 -0400

    ADD: added setpy file

---
+++ setup.py
@@ -0,0 +1,120 @@
+""" VAD: Velocity Azimuth Display
+
+Velocity Azimuth Display derived winds. Contains retrievals
+of the zonal (north/south) and meridional (east/west) wind
+speed components, in precipitating systems, as a function
+of time and height.
+
+"""
+
+import os
+import subprocess
+from setuptools import setup, find_packages
+
+DOCLINES = __doc__.split('\n')
+
+CLASSIFIERS = """\
+Development Status :: 2 - Pre-Alpha
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Programming Language :: Python
+Programming Language :: Python :: 3.6
+Topic :: Scientific/Engineering
+Topic :: Scientific/Engineering :: Atmospheric Science
+Operating System :: POSIX :: Linux
+"""
+
+NAME = 'vad'
+AUTHOR = 'Jason Hemedinger, Zachary Sherman, Scott Collis'
+MAINTAINER = 'Data Informatics and Geophysical Retrievals (DIGR)'
+MAINTAINER_EMAIL = 'jhemedinger@anl.gov'
+DESCRIPTION = DOCLINES[0]
+LONG_DESCRIPTION = '\n'.join(DOCLINES[2:])
+URL = 'https://github.com/ANL-DIGR/SAPR-QVP-VAP'
+LICENSE = 'BSD'
+CLASSIFIERS = filter(None, CLASSIFIERS.split('\n'))
+MAJOR = 0
+MINOR = 1
+MICRO = 0
+ISRELEASED = False
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+# Return the git revision as a string
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+
+def write_version_py(filename='vad/version.py'):
+    cnt = """
+# THIS FILE IS GENERATED FROM PYART SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+if not release:
+    version = full_version
+"""
+    # Adding the git rev number needs to be done inside write_version_py(),
+    # otherwise the import of vad.version messes up the build under Python 3.
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    elif os.path.exists('vad/version.py'):
+        # must be a source distribution, use existing version file
+        try:
+            from vad.version import git_revision as GIT_REVISION
+        except ImportError:
+            raise ImportError("Unable to import git_revision. Try removing "
+                              "vad/version.py and the build directory "
+                              "before building.")
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev+' + GIT_REVISION[:7]
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version': FULLVERSION,
+                       'git_revision': GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url=URL,
+    author=AUTHOR,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    packages=find_packages())
